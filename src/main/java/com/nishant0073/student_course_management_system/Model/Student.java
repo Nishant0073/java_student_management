d40cf1e5b44c5752c36263053892e8f799cfbffd
Nishant Shingate <nishantks121@gmail.com>
2025-09-24 20:53:48 +0530

    removed unnecessory code

--- src/main/java/com/nishant0073/student_course_management_system/Model/Student.java
+++ src/main/java/com/nishant0073/student_course_management_system/Model/Student.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonView;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
