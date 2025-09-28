f2f8946811e68af3c063fb93663817804fcb5de6
Nishant Shingate <nishantks121@gmail.com>
2025-09-28 19:36:39 +0530

    jpl demo

--- src/main/java/com/nishant0073/student_course_management_system/Repository/StudentRepository.java
+++ src/main/java/com/nishant0073/student_course_management_system/Repository/StudentRepository.java
@@ -2,11 +2,12 @@ package com.nishant0073.student_course_management_system.Repository;
 import org.springframework.stereotype.Repository;
 import com.nishant0073.student_course_management_system.Model.Student;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
-    public Page<Student> findStudentByEmail(String email,Pageable pageable);
+    @Query("SELECT u FROM Student u WHERE u.email = :emailId")
+    public Student findStudentByEmail(@Param("emailId") String emailId);
 } 
